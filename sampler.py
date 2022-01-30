0660f6a9276d03cbd75c7c094040c9ab7420ee6c
Lingle, Micah A <mlingle@gatech.edu>
2022-01-29 19:59:11 -0500

    Added RMS noise removal
    Compute root mean square noise and remove it from a sample

--- sampler.py
+++ sampler.py
@@ -7,6 +7,7 @@ import wave
 from scipy.io import wavfile
 import librosa
 import numpy as np
+import argparse as ap
 
 desiredRate = 22050
 
@@ -32,10 +33,10 @@ def getPaths(p = "./datasets/"):
 	print("Recursively found files: ",paths)
 	return paths
 	
-def aggAnalytics(paths):
+def aggAnalytics(path):
 	
-	waveAnalytics(paths[0])		# open the first file only for debug
-	skWaveAnalytics(paths[0])
+	waveAnalytics(path)		# open the first file only for debug
+	#skWaveAnalytics(path)
 	
 # python's wave analytics
 def waveAnalytics(path : str):
@@ -48,6 +49,7 @@ def waveAnalytics(path : str):
 # SciKit's wave analytics
 def skWaveAnalytics(path):
 	rate, data = wavfile.read(path)
+	print(f"File name: {path}")
 	print("Sampling (frame) rate = ", rate)
 	print("Total samples (frames) = ", data.shape)
 	print(data)
@@ -55,20 +57,32 @@ def skWaveAnalytics(path):
 # Use librosa to get frequency bands
 def getLibr(path):
 	wav, sr = librosa.load(path, sr = desiredRate)
+	wav = rmRmsNoise(wav)
+	print(len(wav))
+	for f in wav:
+		print(f)
+
 	
-def rmRmsNoise(wav, sr):
+def rmRmsNoise(wav):
 	# Compute rms amplitude
-	rms = np.sqrt(np.average(np.square(wav)))
+	rms = np.sqrt(np.average(np.square(wav))) 
+	print(f"rms is {rms}")
 	# Based on absolute value of amplitude, zero out quiet bits
 	abs = np.abs(wav)
 	return np.where(abs > rms, wav, 0)
 	
 def main():
 	paths = getPaths()
-	aggAnalytics(paths)
+	waveAnalytics(paths[0])
 
-def test():
-	main()
+	print(paths[0])
+	getLibr(paths[0])
 
 if (__name__ == "__main__"):
-	test()
+	parser = ap.ArgumentParser()
+	parser.add_argument("sample_rate", type = int)
+	args = parser.parse_args()
+	print(args.sample_rate)
+	desiredRate = args.sample_rate
+
+	main()
