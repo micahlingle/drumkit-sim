b50ba7e266489260b3beb2b370213cbeff76ba38
micahlingle <linglemicah@gmail.com>
2025-11-13 22:34:04 -0800

    Add librosa spectral features

--- src/audio/extraction.py
+++ src/audio/extraction.py
@@ -2,6 +2,7 @@ import numpy as np
 from src.audio.segmentation import SIXTEENTH_MIN_LENGTH_SEC, calculate_rms
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+import librosa
 
 def segments_to_features(
     data: np.ndarray,
@@ -9,19 +10,46 @@ def segments_to_features(
     sr: int,
     segment_length=SIXTEENTH_MIN_LENGTH_SEC,
     debug: bool = False,
-):
+) -> list[np.ndarray]:
     '''
     Convert audio segments to features
     '''
-    ffts = segments_to_ffts(data, segments, sr, segment_length, debug)
-    features = extract_peaks(ffts, n=8)
-    return features
+    # ffts = segments_to_ffts(data, segments, sr, segment_length, debug)
+    # features = extract_peaks(ffts, n=8)
+    # return features
+
+    return extract_centroids(data, segments, sr, debug)
+
+
+def extract_centroids(
+    data: np.ndarray,
+    segments,
+    sr: int,
+    debug: bool = False,
+) -> list[np.ndarray]:
+    '''
+    Extract the centroid of each FFT
+
+    x represents frequency in Hz
+    y represents power
+    '''
+    centroids = []
+    for start, stop in segments:
+        audio_segment = data[start:stop]
+        centroid = librosa.feature.spectral_centroid(y=audio_segment, sr=sr)
+        bandwidth = librosa.feature.spectral_bandwidth(y=audio_segment, sr=sr)
+        flatness = librosa.feature.spectral_flatness(y=audio_segment)
+        rolloff = librosa.feature.spectral_rolloff(y=audio_segment, sr=sr)
+        # TODO: add more librosa built in features since they're free
+        features = np.stack([centroid, bandwidth, flatness, rolloff], axis=-1).flatten()
+        centroids.append(features)
+    return centroids
 
 
 def extract_peaks(
     ffts: list[np.ndarray],
     n: int
-):
+) -> list[np.ndarray]:
     '''
     Convert each fft into a feature vector, where each feature vector contains the frequency of a peak and the amplitude.
     This simplifies the entire FFT (thousands of points) into the most important component frequencies which characterize
