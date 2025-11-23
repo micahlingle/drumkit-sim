1dfbc31485b80cec84c6dce7e40e1cf1f98ff44c
micahlingle <linglemicah@gmail.com>
2025-11-23 10:49:14 -0800

    Remove low-end boost

--- src/audio/preprocessing.py
+++ src/audio/preprocessing.py
@@ -1,6 +1,5 @@
 import numpy as np
 import noisereduce as nr
-from scipy import signal
 from scipy.io import wavfile
 import wave
 import numpy as np
@@ -14,33 +13,9 @@ def load_audio(path: str) -> tuple[np.ndarray, int]:
 
 
 def clean_audio(data: np.ndarray, sr: int):
-    """
-    1. Noise reduce
-    2. Bandpass filter
-        - Eliminate powerful high frequencies which are likely noise.
-        - Save the audio at this point as we will use it later to check amplitudes.
-    """
     no_noise = nr.reduce_noise(y=data, sr=sr)
     wavfile.write("debug/reduced_noise.wav", sr, no_noise)
-
-    sos = signal.butter(5, [200, 5000], "bandpass", fs=sr, output="sos")
-    filtered = signal.sosfilt(sos, no_noise)
-    return filtered
-
-
-def boost_low_end(
-    data: np.ndarray, cutoff_freq: int = 100, amplification_factor: float = 3
-):
-    # Define the cutoff frequency and amplification factor
-
-    # Create a mask for low frequencies
-    # Use abs(xf) to cover both positive and negative frequency components (for real signal symmetry)
-    low_freq_mask = np.abs(data) < cutoff_freq
-
-    # Amplify the selected frequencies
-    yf_amplified = data.copy()
-    yf_amplified[low_freq_mask] *= amplification_factor
-    return yf_amplified
+    return no_noise
 
 
 def print_wave_analytics(path: str):
